cd044c28f62c976e9df3f26b37a951449c6ecb3a
jac08h <jac08h@protonmail.com>
2020-06-05 14:53:58 +0200

    header of PokerHand class

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include "card.h"
+#include "pokerhand.h"
+
+enum class Result { Win, Loss, Tie };
+
+Result compare (const PokerHand &player, const PokerHand &opponent) {
+  return Result::Loss;
+}
+
 
 int main()
 {
     Card p = Card{"3D"};
+
     std::cout << p << std::endl;
+    PokerHand hand ("KS 2H 5C JD TD");
 }
---
+++ src/pokerhand.h
@@ -0,0 +1,26 @@
+#ifndef POKERHAND_H
+#define POKERHAND_H
+
+#include <map>
+#include <vector>
+#include "card.h"
+
+class PokerHand
+{
+private:
+    std::vector<Card> m_cards;
+
+    bool is_straight();
+    bool is_flush();
+    bool is_full_house();
+    bool is_x_of_a_kind(int x);
+    bool is_two_pair();
+
+    int highest_card();
+
+public:
+    PokerHand(const char* pokerhand) {};
+    int rank_card(); // 1 (high card) - 10 (royal flush)
+};
+
+#endif // POKERHAND_H
